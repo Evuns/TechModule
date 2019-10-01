bdaeab56dd35b40a77c79288c8417f6ba58af498
Evuns <m_ateva@abv.bg>
2019-10-01 21:21:25 +0300

    4.PrintAndSum

---
+++ 01.JavaIntroduction/src/JavaIntroductionExercise/PrintSum.java
@@ -0,0 +1,21 @@
+package JavaIntroductionExercise;
+
+import java.util.Scanner;
+
+public class PrintSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            sum += i;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Sum: " + sum);
+    }
+}
+
