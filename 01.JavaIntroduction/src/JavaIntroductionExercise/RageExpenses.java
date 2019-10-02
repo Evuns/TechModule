e595325ddecda49d029907e34f4d814d823ead93
Evuns <m_ateva@abv.bg>
2019-10-02 20:42:18 +0300

    10.RageExpenses

---
+++ 01.JavaIntroduction/src/JavaIntroductionExercise/RageExpenses.java
@@ -0,0 +1,39 @@
+package JavaIntroductionExercise;
+
+import java.util.Scanner;
+
+public class RageExpenses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        int lostGames = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+        int headsetCount = 0;
+        int mouseCount = 0;
+        int keyboardCount = 0;
+        int displayCount = 0;
+
+        for (int i = 1; i <= lostGames ; i++) {
+            if(i % 2 == 0){
+                headsetCount ++;
+            }
+            if(i % 3 == 0){
+                mouseCount ++;
+            }
+            if (i % 2 == 0 & i % 3 == 0){
+                keyboardCount ++;
+                if(keyboardCount % 2 == 0){
+                    displayCount ++;
+                }
+            }
+
+        }
+
+        double sum = headsetPrice * headsetCount + mousePrice * mouseCount + keyboardPrice * keyboardCount + displayPrice * displayCount;
+        System.out.printf("Rage expenses: %.2f lv.", sum);
+
+    }
+}
