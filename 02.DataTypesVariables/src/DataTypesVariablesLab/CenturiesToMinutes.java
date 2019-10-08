3d278992e0868edb01330f00d20cff410e499a78
Evuns <m_ateva@abv.bg>
2019-10-08 20:54:35 +0300

    9.CenturiesToMinutes

---
+++ 02.DataTypesVariables/src/DataTypesVariablesLab/CenturiesToMinutes.java
@@ -0,0 +1,17 @@
+package DataTypesVariablesLab;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int centuries = Integer.parseInt(scanner.nextLine());
+        int years = centuries * 100;
+        double days = Math.floor(years * 365.2422);
+        double hours = days * 24;
+        BigDecimal minutes = new BigDecimal(hours).multiply(BigDecimal.valueOf(60));
+        System.out.printf("" + centuries + " centuries = " + years + " years = %.0f" + " days = %.0f" + " hours = " + minutes +" minutes",days, hours);
+    }
+}
