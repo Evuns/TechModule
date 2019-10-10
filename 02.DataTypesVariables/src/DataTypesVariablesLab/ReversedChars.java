ea9c02fad485dd668a8371df2a94640d9c258f39
Evuns <m_ateva@abv.bg>
2019-10-10 18:08:36 +0300

    7.ReversedChars

---
+++ 02.DataTypesVariables/src/DataTypesVariablesLab/ReversedChars.java
@@ -0,0 +1,15 @@
+package DataTypesVariablesLab;
+
+import java.util.Scanner;
+
+public class ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char a = scanner.nextLine().charAt(0);
+        char b = scanner.nextLine().charAt(0);
+        char c = scanner.nextLine().charAt(0);
+
+        System.out.println("" + c +" " + b + " " + a);
+    }
+}
