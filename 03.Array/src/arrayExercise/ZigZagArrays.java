362ac15910871ca6af6894c3263ca68afdc94e23
Evuns <m_ateva@abv.bg>
2019-11-16 19:07:56 +0200

    3.ArraysRotation

---
+++ 03.Array/src/arrayExercise/ZigZagArrays.java
@@ -0,0 +1,30 @@
+package arrayExercise;
+
+import java.util.Scanner;
+
+public class ZigZagArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lines = Integer.parseInt(scanner.nextLine());
+
+        String[] firstArray = new String[lines];
+        boolean arrayOne = true;
+        String[] secondArray = new String[lines];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            if (arrayOne) {
+                firstArray[i] = input[0];
+                secondArray[i] = input[1];
+                arrayOne = false;
+            } else {
+                secondArray[i] = input[0];
+                firstArray[i] = input[1];
+                arrayOne = true;
+            }
+        }
+        System.out.println(String.join(" ", firstArray));
+        System.out.println(String.join(" ", secondArray));
+    }
+}
