9f3073b69b8bd1b59449e2519a10a0399da6b62e
Evuns <m_ateva@abv.bg>
2019-12-31 14:57:04 +0200

    1.AdvertisementMessage

--- 06.ObjectsAndClasses/src/Exercise/AdvertisementMessage.java
+++ 06.ObjectsAndClasses/src/Exercise/AdvertisementMessage.java
@@ -10,10 +10,8 @@ public class AdvertisementMessage{
 
         String [] phrases = {"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.",
                 "Exceptional product.", "I can’t live without this product."};
-
         String [] events = {"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!",
                 "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
-
         String [] authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
 
         String [] city = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
@@ -29,4 +27,4 @@ public class AdvertisementMessage{
 
         }
     }
-}	
+}
